4dc7f9600da2f3785415bb72fdae2f04f566030e
0xSandro <107639780+0xSandro@users.noreply.github.com>
2026-06-15 15:31:20 +0200

    Delete newtab.js

--- newtab.js
+++
@@ -1,3 +0,0 @@
-// Tessra has been refactored into ES modules under ./src/.
-// Entry point: src/main.js (loaded via <script type="module"> in newtab.html).
-// This file is no longer used and can be deleted.
